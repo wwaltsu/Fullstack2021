edc93bafc1b81a7060b0147c9e79d67254392389
wwaltsu <walter.ruoppa@metropolia.fi>
2021-07-04 14:44:59 +0300

    1.2 kurssitiedot done

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,23 +1,62 @@
 import React from "react";
 
-
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Moi sinun nimesi on {props.name} ja olet {props.age} vuotta vanha  
-      </p>
-    </div>
-  )
-}
-
 const App = () => {
+  
+  const kurssi = 'Sovelluskehitys Half Stack'
+  const osa1 = 'Reaktin perusteet'
+  const harjoitukset1 = 10
+  const osa2 = 'Propsien käyttö datan siirtämiseen'
+  const harjoitukset2 = 7
+  const osa3 = 'Komponentin tila'
+  const harjoitukset3 = 14
+
+  const Header = (props) => {
+    return(
+      <div>
+        <h1>{props.kurssi}</h1>
+      </div>
+    )
+  }
+
+  const Part = (props) => {
+    <div>
+      {props.osa}
+    </div>
+  }
+
+  const Content = () => {
+    return (
+      <div>
+        <Part osa={osa1}/>
+        <Part osa={osa1}/>
+        <Part osa={osa1}/>
+      </div>
+    )
+  }
+
+  const Total = (props) => {
+    return (
+      <div>
+        Tehtäviä on yhteensä {props.yhteensä}
+      </div>
+    )
+  }
+ 
+
+  
+
   return (
   <div>
-    <Hello name='Waltsu' age='12'/>
-    <Hello name='Mikael' age='11'/>
+    <Header kurssi={kurssi}/>
+    <Content 
+    osa1={osa1}
+    osa2={osa2} 
+    osa3={osa3}
+    />
+    
+    <Total yhteensä={harjoitukset1+harjoitukset2+harjoitukset3}/>
   </div>
   )
-}
+};
 
 export default App;
