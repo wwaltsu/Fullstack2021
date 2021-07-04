bdae9247549e86f7f12b0180caa2150690a5413c
wwaltsu <walter.ruoppa@metropolia.fi>
2021-07-04 18:20:39 +0300

    1.5 tehty, vaati vain tarkempaa kohdistamista parts.map --> course.parts.map

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,37 +1,37 @@
 import React from "react";
 
 const App = () => {
-  const course = "Half Stack application devolopment";
-  const osat = [
-    {
-      nimi: "Reaktin perusteet",
-      tehtävät: 10,
-    },
-    {
-      nimi: "Propsien harjoittelu",
-      tehtävät: 7,
-    },
-    {
-      nimi: "Komponentin tila",
-      tehtävät: 14,
-    },
-  ];
-  
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
   return (
-    
     <div>
-      <h1>{course}</h1>
-      {osat.map(osat => {
+      <h1>{course.name}</h1>
+      {course.parts.map((osat) => {
         return (
           <p>
-            {osat.nimi} {osat.tehtävät}
+            {osat.name} {osat.exercises}
           </p>
-        )
+        );
       })}
-      
-        Tehtäviä yhteensä {osat.reduce((sum,osat) => sum + osat.tehtävät,0)}
-
-        {/*
+      Tehtäviä yhteensä {course.parts.reduce((sum, osat) => sum + osat.exercises, 0)}
+      {/*
       <p>
         Tehtäviä yhteensä {osat.reduce(function(sum,osat) {
           return sum + osat.tehtävät
@@ -40,9 +40,7 @@ const App = () => {
       </p>
       */}
     </div>
-  )
-
-  };
-  
+  );
+};
 
 export default App;
