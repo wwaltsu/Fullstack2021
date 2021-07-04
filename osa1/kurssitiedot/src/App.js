1a9c43861458c1e03c2dbb18b9f5da5b6e945879
wwaltsu <walter.ruoppa@metropolia.fi>
2021-07-04 17:19:04 +0300

    1.4 tehty, nyt hyödynsin reduce:a ja map toimintoja

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,32 +1,48 @@
 import React from "react";
 
 const App = () => {
-  const course = "Half Stack Application devolopment";
-  const part1 = {
-    nimi: "Reactin perusteet ",
-    tehtävät: 10,
-  };
-  const part2 = {
-    nimi: "Propsien käyttö datan siirrossa ",
-    tehtävät: 7,
-  };
-  const part3 = {
-    nimi: "Komponentin tila ",
-    tehtävät: 14,
-  };
-
+  const course = "Half Stack application devolopment";
+  const osat = [
+    {
+      nimi: "Reaktin perusteet",
+      tehtävät: 10,
+    },
+    {
+      nimi: "Propsien harjoittelu",
+      tehtävät: 7,
+    },
+    {
+      nimi: "Komponentin tila",
+      tehtävät: 14,
+    },
+  ];
+  
   return (
+    
     <div>
       <h1>{course}</h1>
-      <div>
-        Osiot
-        <p> {part1.nimi} tehtäviä {part1.tehtävät}</p>
-        <p> {part2.nimi} tehtäviä  {part2.tehtävät}</p>
-        <p> {part3.nimi} tehtäviä {part3.tehtävät}</p>
-        <p>Yhteensä tehtäviä {part1.tehtävät + part2.tehtävät + part3.tehtävät}</p>
-      </div>
+      {osat.map(osat => {
+        return (
+          <p>
+            {osat.nimi} {osat.tehtävät}
+          </p>
+        )
+      })}
+      
+        Tehtäviä yhteensä {osat.reduce((sum,osat) => sum + osat.tehtävät,0)}
+
+        {/*
+      <p>
+        Tehtäviä yhteensä {osat.reduce(function(sum,osat) {
+          return sum + osat.tehtävät
+        }, 0)
+        }
+      </p>
+      */}
     </div>
-  );
-};
+  )
+
+  };
+  
 
 export default App;
