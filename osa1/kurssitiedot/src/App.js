2f497ec3bd9c12f01ac456c61c5cc92da9c0557f
wwaltsu <walter.ruoppa@metropolia.fi>
2021-07-07 00:28:59 +0300

    Kurssitietoihin lisätty viimeinen osa 1.6 unicafe done

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,23 +1,43 @@
 import React from "react";
 
+const App = () => {
 
-const Hello = (props) => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
   return (
     <div>
+      <h1>{course.name}</h1>
+      {course.parts.map((osat) =>  <p> {osat.name} {osat.exercises} </p> )}
+      
+      Tehtäviä yhteensä {course.parts.reduce((sum, osat) => sum + osat.exercises, 0)}
+      
+      {/* hyödyntäen funktiota
       <p>
-        Moi sinun nimesi on {props.name} ja olet {props.age} vuotta vanha  
+        Tehtäviä yhteensä {osat.reduce(function(sum,osat) {
+          return sum + osat.tehtävät
+        }, 0)
+        }
       </p>
-    </div>
-  )
-}
+      */}
 
-const App = () => {
-  return (
-  <div>
-    <Hello name='Waltsu' age='12'/>
-    <Hello name='Mikael' age='11'/>
-  </div>
-  )
-}
+    </div>
+  );
+};
 
 export default App;
--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
+const App = () => {
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handlNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  const Button = ( { handleClick, text} ) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button handleClick={handleGoodClick} text='Hyvä'/>
+      <Button handleClick={handlNeutralClick} text='Neutraali'/>
+      <Button handleClick={handleBadClick} text='Huono'/>
+    <h3>
+      statistics
+    </h3>
+    <p>Hyvä : {good} </p>
+    <p>Neutraali : {neutral}</p>
+    <p>Huono : {bad}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
