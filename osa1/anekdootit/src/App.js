4eaa1e7087c0b9795b42d86563fbbf37e1a9f365
wwaltsu <walter.ruoppa@metropolia.fi>
2021-07-07 01:46:47 +0300

    1.12 anekdootti valmis

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const Button = ({ text, handleClick }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleClick}> {text}</button>
     </div>
   );
-}
+};
+
+/*
+  <div>
+    <p> {anecdotes[selected]} </p>
+    <button
+    onClick={() => setSelected(Math.floor(Math.random() * 7))}
+    >
+    {console.log(selected)}
+    Seuraava anekdootti
+    </button>
+  </div>
+*/
+
+
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const random = () => setSelected(Math.floor(Math.random() * 7))
+  return (
+    <div>
+      <p>{anecdotes[selected]}</p>
+     <Button
+     handleClick={random}
+     text='Seuraava anekdoottisi'
+     />
+    </div>
+  );
+};
 
 export default App;
