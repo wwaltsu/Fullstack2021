50ffb00982723a0f78cddb9d768beb36d4e43d23
wwaltsu <walter.ruoppa@metropolia.fi>
2021-07-07 01:10:37 +0300

    unicafe viimeinen versio tuotu + index:t sisälsi turhia rivejä

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const StatisticsLine = ({ text,value }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <p>{text} {value}</p>
     </div>
   );
-}
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [allClicks, setAllClicks] = useState(0);
+
+  const handleGoodClick = () => {
+    setAllClicks(allClicks + 1);
+    setGood(good + 1);
+  };
+
+  const handlNeutralClick = () => {
+    setAllClicks(allClicks + 1);
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setAllClicks(allClicks + 1);
+    setBad(bad + 1);
+  };
+
+  const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>{text}</button>
+  );
+
+
+  if(allClicks === 0){
+    return (
+      <div>
+      
+      <Button handleClick={handleGoodClick} text="Hyvä" />
+      <Button handleClick={handlNeutralClick} text="Neutraali" />
+      <Button handleClick={handleBadClick} text="Huono" />
+      <h3>Tilastot</h3>
+      <p>Ei arvosteluja vielä</p>
+      </div>
+    )}
+    
+    else
+
+
+  return (
+    
+    <div>
+      <Button handleClick={handleGoodClick} text="Hyvä" />
+      <Button handleClick={handlNeutralClick} text="Neutraali" />
+      <Button handleClick={handleBadClick} text="Huono" />
+      <StatisticsLine text='hyvä' value={good}/>
+      <StatisticsLine text='keskiverto' value={neutral}/>
+      <StatisticsLine text='huono' value={bad}/>
+      <h3>Statistics</h3>
+      <StatisticsLine text='palautteiden määrä' value={allClicks}/>
+      <StatisticsLine text='keskiarvo' value={(good+bad)/allClicks}/>
+      <StatisticsLine text='positiivisa'value={(good/allClicks)}/>
+      
+      
+    </div>
+  );
+};
 
 export default App;
+
