6280aec3c3c93f451e0b1f42ae6d2e3b22dbba2c
Walter R <waltsu123123@gmail.com>
2021-07-07 14:45:21 +0300

    esimerkkejä

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+import Note from "./Note";
 
-
-const App = () => {
+const App = ({ notes }) => {
   return (
     <div>
-
+      <h1>Muistiinpanot</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
 export default App;
