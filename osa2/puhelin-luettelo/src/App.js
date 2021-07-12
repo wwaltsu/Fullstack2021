833b024c2644a689e6af722d086e9915773d15b3
wwaltsu <walter.ruoppa@metropolia.fi>
2021-07-12 23:48:38 +0300

    2,7 uupuu muut tehty

--- osa2/puhelin-luettelo/src/App.js
+++ osa2/puhelin-luettelo/src/App.js
@@ -1,22 +1,32 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto hellas" }]);
+  const [persons, setPersons] = useState([
+    {
+      name: "Arto hellas",
+      number: "062343212",
+    },
+  ]);
 
   const [name, setName] = useState("");
-
+  const [number, setNumber] = useState("");
+  const [filter, setFilter] = useState("")
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(name);
-    addPerson(name);
+    addPerson(name,number);
   };
 
-  const addPerson = (name) => {
-    setPersons([...persons, { name }]);
+  const addPerson = (name, number) => {
+    setPersons([...persons, { name, number } ]);
   };
 
   return (
     <div>
+      filter shown with:<input
+      value={filter}
+      onChange={(e => setFilter(e.target.value))}
+      />
       <h2>Puhelinluettelo</h2>
       <form onSubmit={handleSubmit}>
         <div>
@@ -29,13 +39,19 @@ const App = () => {
           <button type="submit">add</button>
         </div>
         <div>
-          
+          number:{" "}
+          <input
+            type="text"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
+          />
         </div>
+        <div></div>
       </form>
       <h2>Numbers</h2>
       {/* mapataan henkilöt array of objects [{ name: "Arto hellas" }] */}
-      {persons.map((person) => (
-        <p key={person.name}> {person.name} </p>
+      {persons.filter(profile => profile.name.toLowerCase().includes(filter.toLowerCase())).map((person) => (
+        <p key={person.name}> {person.name} {person.number} </p>
       ))}
       <div></div>
       ...
